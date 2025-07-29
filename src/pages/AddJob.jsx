7b70cb12ff500d516d729046cc6c64a19791c0e8
Nada-ibrahim12 <123187920+Nada-ibrahim12@users.noreply.github.com>
2025-07-29 12:45:09 +0300

    add fields

--- src/pages/AddJob.jsx
+++ src/pages/AddJob.jsx
@@ -6,7 +6,7 @@ export class AddJob extends Component {
       <div>
         <h1 className="text-2xl font-bold text-center mt-10">Add Job</h1>
         <div className="container mx-auto shadow-xl shadow-cyan-700 rounded-lg p-10 mt-10">
-          <form className="m-10 grid grid-cols-1 md:grid-cols-2 sm:grid-cols-1 gap-x-10 gap-y-6">
+          <form className="grid grid-cols-1 md:grid-cols-2 sm:grid-cols-1 gap-x-10 gap-y-6">
             <div className="mb-4">
               <label className="block text-gray-700">Job Title</label>
               <input
@@ -24,6 +24,22 @@ export class AddJob extends Component {
               />
             </div>
             <div className="mb-4">
+              <label className="block text-gray-700">Status</label>
+              <select className="p-3 border border-gray-300 rounded-md w-full">
+                <option value="applied">Applied</option>
+                <option value="interview">Interview</option>
+                <option value="offer">Offer</option>
+                <option value="rejected">Rejected</option>
+              </select>
+            </div>
+            <div className="mb-4">
+              <label className="block text-gray-700">Application Date</label>
+              <input
+                type="date"
+                className="p-3 border border-gray-300 rounded-md w-full"
+              />
+            </div>
+            <div className="mb-4">
               <label className="block text-gray-700">Location</label>
               <input
                 type="text"
@@ -31,6 +47,7 @@ export class AddJob extends Component {
                 placeholder="Enter job location"
               />
             </div>
+
             <div className="mb-4">
               <label className="block text-gray-700">Description</label>
               <textarea
